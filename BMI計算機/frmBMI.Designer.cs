32aae869f6885f8df8edc80de353baa4cfed0ebe
user <user@1008-048>
2026-03-25 15:33:45 +0800

    Add project files.

---
+++ BMI計算機/frmBMI.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace BMI計算機
+{
+    partial class frmBMI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpInput = new System.Windows.Forms.GroupBox();
+            this.grpOutput = new System.Windows.Forms.GroupBox();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.lblBMI = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.grpInput.SuspendLayout();
+            this.grpOutput.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpInput
+            // 
+            this.grpInput.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.grpInput.Controls.Add(this.btnRun);
+            this.grpInput.Controls.Add(this.txtWeight);
+            this.grpInput.Controls.Add(this.txtHeight);
+            this.grpInput.Controls.Add(this.lblWeight);
+            this.grpInput.Controls.Add(this.lblHeight);
+            this.grpInput.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpInput.Location = new System.Drawing.Point(32, 28);
+            this.grpInput.Name = "grpInput";
+            this.grpInput.Size = new System.Drawing.Size(508, 201);
+            this.grpInput.TabIndex = 0;
+            this.grpInput.TabStop = false;
+            this.grpInput.Text = "輸入";
+            // 
+            // grpOutput
+            // 
+            this.grpOutput.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.grpOutput.Controls.Add(this.lblResult);
+            this.grpOutput.Controls.Add(this.lblBMI);
+            this.grpOutput.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpOutput.Location = new System.Drawing.Point(32, 235);
+            this.grpOutput.Name = "grpOutput";
+            this.grpOutput.Size = new System.Drawing.Size(508, 201);
+            this.grpOutput.TabIndex = 1;
+            this.grpOutput.TabStop = false;
+            this.grpOutput.Text = "計算結果";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(30, 39);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(74, 20);
+            this.lblHeight.TabIndex = 2;
+            this.lblHeight.Text = "身高(cm)";
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Location = new System.Drawing.Point(30, 128);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(70, 20);
+            this.lblWeight.TabIndex = 3;
+            this.lblWeight.Text = "體重(kg)";
+            // 
+            // lblBMI
+            // 
+            this.lblBMI.AutoSize = true;
+            this.lblBMI.Location = new System.Drawing.Point(30, 104);
+            this.lblBMI.Name = "lblBMI";
+            this.lblBMI.Size = new System.Drawing.Size(145, 20);
+            this.lblBMI.TabIndex = 4;
+            this.lblBMI.Text = "身體質量指數(BMI)";
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Location = new System.Drawing.Point(196, 104);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(202, 25);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtHeight.Location = new System.Drawing.Point(122, 36);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(161, 29);
+            this.txtHeight.TabIndex = 5;
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtWeight.Location = new System.Drawing.Point(122, 125);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(161, 29);
+            this.txtWeight.TabIndex = 6;
+            // 
+            // btnRun
+            // 
+            this.btnRun.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnRun.Location = new System.Drawing.Point(340, 36);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(138, 118);
+            this.btnRun.TabIndex = 7;
+            this.btnRun.Text = "計算";
+            this.btnRun.UseVisualStyleBackColor = false;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // frmBMI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grpOutput);
+            this.Controls.Add(this.grpInput);
+            this.Name = "frmBMI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BMI計算機";
+            this.grpInput.ResumeLayout(false);
+            this.grpInput.PerformLayout();
+            this.grpOutput.ResumeLayout(false);
+            this.grpOutput.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpInput;
+        private System.Windows.Forms.GroupBox grpOutput;
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblBMI;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtHeight;
+    }
+}
+
---
+++ BMI計算機/frmBMI.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI計算機
+{
+    public partial class frmBMI : Form
+    {
+        public frmBMI()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            bool isHeightValid = double.TryParse(txtHeight.Text, out double height);
+            bool isWeightValid = double.TryParse(txtWeight.Text, out double weight);
+
+            //驗證身高輸入
+            if(!isHeightValid)
+            {
+                MessageBox.Show("請輸入有效的身高數值");
+                return;
+            }
+            //驗證體重輸入
+            if(!isWeightValid)
+            {   
+                if(weight <= 0)
+                {
+                    MessageBox.Show("體重數值必須大於0");
+                    return;
+                }
+
+            }
+            height = height / 100; // Convert cm to m
+
+            double bmi = weight / (height * height);
+            
+            string strResult = "";
+
+            if (bmi < 18.5)
+            {
+                strResult = "體重過輕";
+            }
+            else if (bmi >= 18.5 && bmi < 24)
+            {
+                strResult = "正常範圍";
+            }
+            else if (bmi >= 24 && bmi < 27)
+            {
+                strResult = "過重";
+            }
+            else if (bmi >= 27 && bmi < 30)
+            {
+                strResult = "輕度肥胖";
+            }
+            else if (bmi >= 30 && bmi < 35)
+            {
+                strResult = "中度肥胖";
+            }
+            else
+            {
+                strResult = "重度肥胖";
+            }
+
+        }
+
+    }
+}
